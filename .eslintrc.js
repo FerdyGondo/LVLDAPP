21d60e4b2493622ad6809de1c527f49e53700da8
Reed Hazen <redhazen@gmail.com>
2020-09-20 20:13:49 -0700

    upload logo svg as well as some needed libraries and updated eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,48 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+    ecmaVersion: 6,
+    sourceType: "module",
+    jsx: true,
+    ecmaFeatures: {
+      jsx: true,
+      restParams: true,
+      spread: true,
+      experimentalObjectRestSpread: true,
+      modules: true
+    }
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  extends: [
+    "plugin:prettier/recommended",
+    "prettier/react",
+    "plugin:react/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: ["babel", "react", "prettier", "react-native-globals"],
+  env: {
+    "react-native-globals/all": true
+  },
+  globals: {
+    fetch: true,
+    __DEV__: true
+  },
+  rules: {
+    "@typescript-eslint/camelcase": 0,
+    "@typescript-eslint/explicit-function-return-type": ["off"],
+    "@typescript-eslint/explicit-member-accessibility": ["warn", { accessibility: "no-public" }],
+    "@typescript-eslint/no-use-before-define": ["off"],
+    "@typescript-eslint/no-explicit-any": ["off"],
+    "@typescript-eslint/no-var-requires": ["off"],
+    "react/prop-types": ["off"],
+    "react/no-unescaped-entities": ["off"],
+    "react/display-name": ["warn"]
+  }
 };
