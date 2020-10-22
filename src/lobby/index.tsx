452b59143475f166a64a900d08ac8392024a7622
Ebuka Umeh <charpellumeh@gmail.com>
2020-10-22 16:47:16 +0100

    Feauture LA-35 Lobby Chat (#30)

--- src/lobby/index.tsx
+++ src/lobby/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Dimensions } from 'react-native'
+import { Dimensions, KeyboardAvoidingView, Platform } from 'react-native'
 import styled from 'styled-components'
 import ProfileIcon from '../../assets/svg/ProfileIcon'
 import { useSelector, useDispatch } from 'react-redux'
@@ -8,7 +8,9 @@ import Loading from '../shared/components/Loading'
 
 const {width,height} = Dimensions.get("window")
 
+const messages = [{userId: "rhazen", name: "Reed H.", message: "Who is ready to play?", createdAt: Date.now()},{userId: "jspaits", name: "Jason S.", message: "Been practicing all day", createdAt: Date.now()},{userId: "eumeh", name: "Ebuka U.", message: "Who is ready to play?", createdAt: Date.now()}]
 
+const userId = "eumeh";
 type Props = {
     route: () => {};
 }
@@ -18,6 +20,8 @@ export default function index({ route, navigation }: Props) {
     const { entry, lobbyItem } = route?.params
     const dispatch = useDispatch()
     const users = useSelector(state => state.users.list)
+    const [message, setMessage] = useState('')
+    const [messaged, setMessaged] = useState(messages)
 
     useEffect(() => {
         dispatch(Actions.users.fetchUsers.trigger())
@@ -25,6 +29,23 @@ export default function index({ route, navigation }: Props) {
 
     const lobbySwitch = (data: string): void => {
         setLobby(data)
+    }
+
+    const onSubmit = () => {
+        if (!message) return 
+        const formData = {
+            userId: "eumeh",
+            name: "Ebuka U.",
+            message: message,
+            createdAt: Date.now()
+        }
+        setMessaged(messaged => ([...messaged,formData, ]))
+        setMessage('')
+        
+    }
+
+    const isUser = (item: {}) => {
+        return item.userId === userId;
     }
 
     const renderList = ({ item }) => {
@@ -41,6 +62,31 @@ export default function index({ route, navigation }: Props) {
         )
     }
 
+    const renderMessageList = ({ item }) => {
+        return (
+            <MessageTextContainer>
+                {!isUser(item) ? (
+                    <>
+                    <MessageName>{item.name}</MessageName>
+                    <MessageContainer>
+                            <ProfileIcon width={38} />
+                            <MessageBorderContainer>
+                                <MessageText>{item.message}</MessageText>
+                            </MessageBorderContainer>
+                    </MessageContainer>
+                    </>
+                ) : (
+                    <PetashContainer>
+                            <MessageBorderContainer isUser={isUser(item)}>
+                                <MessageText isUser={isUser(item)}>{item.message}</MessageText>
+                            </MessageBorderContainer>
+                    </PetashContainer>
+                )}
+            </MessageTextContainer>
+
+        )
+    }
+
     const mappedData = () => {
         return users.reduce((acc, curr) => {
             return acc.concat(curr.account)
@@ -48,6 +94,31 @@ export default function index({ route, navigation }: Props) {
     }
 
     if (!users) return <Loading />
+
+    const renderItem = () => {
+        if (lobby === "lobby") {
+            return <List 
+            data={mappedData()}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={renderList}
+        />
+        }
+        return (
+            <>
+            <List 
+                    data={messaged}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={renderMessageList}
+                />
+            <BoxContainer>
+                <EnterMessage placeholder={'Enter Message'} onChangeText={(text) => setMessage(text)} value={message} />
+                <SendContainer onPress={() => onSubmit()}>
+                    <MessageText color={'white'}>Send</MessageText>
+                </SendContainer>
+            </BoxContainer>
+            </>
+        )
+    }
 
     return (
         <Container>
@@ -101,11 +172,7 @@ export default function index({ route, navigation }: Props) {
                         <SecondText lobby={lobby}>{`Chat`}</SecondText>
                     </ChatMainContainer>
                 </LobbyContainer>
-                <List 
-                    data={mappedData()}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={renderList}
-                />
+                {renderItem()}
         </Container>
     )
 }
@@ -249,3 +316,51 @@ const Play = styled.TouchableOpacity`
     border-width: 0.5px;
     padding: 5px;
 `
+const MessageContainer = styled.View`
+    flex-direction: row;
+    align-items: center;
+`
+const PetashContainer = styled.View`
+    align-items: flex-end;
+`
+const MessageTextContainer = styled.View`
+    margin: 15px 25px 0px;
+`
+const MessageBorderContainer = styled.View`
+    background-color: ${props => props.isUser ? "#000" : "#D6D6D6"};
+    padding: 10px;
+    margin-left: 10px;
+    border-radius: 5px;
+    
+`
+const MessageName = styled.Text`
+    font-family: "Montserrat-Bold";
+    font-size: 12px;
+    opacity: 0.4;
+    margin: 0px 55px 3px;
+`
+const MessageText = styled.Text`
+    font-family: "Montserrat-Medium";
+    font-size: 12px;
+    color: ${props => props.color === "white" || props.isUser ? "#fff" : "#000"}
+`
+const BoxContainer = styled.View`
+    padding: 10px 20px;
+    background-color: #D6D6D6;
+    flex-direction: row;
+    align-items: center;
+`
+const EnterMessage = styled.TextInput`
+    background-color: #fff;
+    flex: 1;
+    height: 40px;
+    border-radius: 20px;
+    padding-left: 10px;
+`
+const SendContainer = styled.TouchableOpacity`
+    margin-left: 15px;
+    background-color: #000;
+    padding: 10px 15px;
+    border-radius: 20px;
+`
+
