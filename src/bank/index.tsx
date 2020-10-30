99cd7988caa3246e356e1dd8c78faf23b9b8b789
Ebuka Umeh <charpellumeh@gmail.com>
2020-10-30 16:02:39 +0100

    feauture LA-78 Bank Sign In (#45)

--- src/bank/index.tsx
+++ src/bank/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Search from '../shared/components/Search'
 import Icons from 'react-native-vector-icons/FontAwesome';
 const myIcon = <Icons name="angle-right" size={25} color={"#979797"} />;
+import BankModal from './components/BankModal';
 
 const data = [{name: "Chase", logo: require('../../assets/images/banks/chase.png')},{name: "Bank of America", logo: require('../../assets/images/banks/america.png')},{name: "Wells Fargo", logo: require("../../assets/images/banks/wells.png")},{name: "PNC", logo: require("../../assets/images/banks/pnc.png")},{name: "TD Bank", logo: require('../../assets/images/banks/td.png')},{name: "U.S Bank", logo: require('../../assets/images/banks/us.png')},{name: "Navy Federal", logo: require('../../assets/images/banks/navy.png')}]
 
 export default function index() {
+    const [modalVisible, setModalVisible] = useState(false)
+    const [bank, setBank] = useState('')
+
+    const openModal = (item) => {
+        setBank(item)
+        setModalVisible(true)
+    }
     const renderList = ({ item }) => {
         return (
-            <BankingContainer>
+            <BankingContainer onPress={() => openModal(item.name)}>
                 <LogoContainer>
                     <Logo source={item.logo} />
                     <TextContainer>{item.name}</TextContainer>
@@ -23,6 +31,7 @@ export default function index() {
 
     return (
         <Container>
+            <BankModal modalVisible={modalVisible} setModalVisible={setModalVisible} text={bank} />
             <Search />
             <List 
                 data={data}
@@ -39,7 +48,7 @@ const Container = styled.View`
 `
 const List = styled.FlatList`
 `
-const BankingContainer = styled.View`
+const BankingContainer = styled.TouchableOpacity`
     background-color: #fff;
     flex-direction: row;
     padding: 12px 17px;
