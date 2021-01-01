a58fb784e4969ebee9524747ec0ad216ccc3822d
Ferdy Gondo <ferdygondo@yahoo.com>
2020-12-31 17:37:15 -0800

    Feature/la 150 handle login signup error (#142)
    * feature/LA-150_Handle_login_signup_error
    
    * feature/LA-150_Handle_login_signup_error
    
    Co-authored-by: Ferdy Gondo <ferdy.gondo@hypermediasystems.com>

--- awsmobile.js
+++ awsmobile.js
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "ca-central-1",
     "aws_cognito_region": "ca-central-1",
-    "aws_user_pools_id": "ca-central-1_2dsLp1yVL",
-    "aws_user_pools_web_client_id": "3hfqshs3cenosj0j3jaggvsqdl",
+    "aws_user_pools_id": "ca-central-1_x8NOndsS0",
+    "aws_user_pools_web_client_id": "4lflck5ahlh1ofkh7tc8cddd1",
     "oauth": {
         "domain": "lvld-api-dev.auth.ca-central-1.amazoncognito.com",
         "redirectSignIn": "myapp://",   
