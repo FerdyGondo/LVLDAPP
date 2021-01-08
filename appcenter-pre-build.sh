945492ec923cd24fa2be8a7551a5fb7cf8988485
Ebuka Umeh <charpellumeh@gmail.com>
2021-01-08 21:48:52 +0100

    Revert "Feauture LA-162 Config for different Enviroment (#148)"
    This reverts commit 14ac66af52d95acb29f8ad4be1576d4a4e99ab70.

--- appcenter-pre-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ${APPCENTER_SOURCE_DIRECTORY}
-echo “USER_POOL_ID=${us-east-1_77fGrGwzg}” >> .env
-echo “USER_POOL_CLIENT_ID=${21bq58t9j7c2r22bbvm23f8dk6}” >> .env
-echo “SANITY_PROJECT_ID=${cqs8qte2}” >> .env
-echo “SANITY_GRAPHQL_URL=${https://cqs8qte2.api.sanity.io/v1/graphql/staging/default}” >> .env
-echo “LVLD_API_BASE_URL=${https://3jbx741ljk.execute-api.us-east-1.amazonaws.com/staging}” >> .env
